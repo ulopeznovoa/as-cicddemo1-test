9f6fe38b1370cc852af6bc759ff3d13eafb57815
Unai Lopez Novoa <ulopeznovoa@users.noreply.github.com>
2024-11-08 17:13:52 +0100

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -3,7 +3,7 @@ class Wallet:
         self.balance = balance
 
     def set_balance(self, val):
-        self.balance = self.balance + val
+        self.balance = self.balance + val + 5
 
     def get_balance(self):
         return self.balance
