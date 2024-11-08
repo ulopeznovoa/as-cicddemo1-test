57610de675e99b7935a0b4d5b7f58bd0c7e82a83
Unai Lopez Novoa <ulopeznovoa@users.noreply.github.com>
2024-11-08 17:41:53 +0100

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,11 @@
+# Definimos una función que suma dos números
+def sumar(a, b):
+    return a + b
+
+# Declaramos dos variables con números a nuestra elección
+numero1 = 5
+numero2 = 7
+
+# Usamos la función sumar y mostramos el resultado con print
+resultado = sumar(numero1, numero2)
+print(f"La suma de {numero1} y {numero2} es: {resultado}")
